6f350028e47880e7b49d4c7770f7e4271fdfe3b3
Sujit <sujeetnangare@gmail.com>
2024-08-25 15:35:00 +0530

    code refactor

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-const addButton = document.querySelector('.addButton');
-var input = document.querySelector('.input');
-const container = document.querySelector('.container');
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.querySelector(".addButton");
+  const input = document.querySelector(".input");
+  const container = document.querySelector(".container");
 
-class item{
-    constructor(itemName){
-
-    this.craeteDiv(itemName);
-}
-
-    craeteDiv(itemName){
-        let input = document.createElement('input');
-        input.value = itemName;
-        input.disabled = true;
-        input.classList.add('item_input');
-        input.type = "text";
-
-        let itemBox = document.createElement('div');
-        itemBox.classList.add('item');
-
-        let editButton = document.createElement('button');
-        editButton.innerHTML = "EDIT";
-        editButton.classList.add('editButton');
-
-        let removeButton = document.createElement('button');
-        removeButton.innerHTML = "REMOVE";
-        removeButton.classList.add('removeButton');
-
-        container.appendChild(itemBox);
-
-        itemBox.appendChild(input);
-        itemBox.appendChild(editButton);
-        itemBox.appendChild(removeButton);
-
-        editButton.addEventListener('click', () => this.edit(input));
-
-        removeButton.addEventListener('click', () => this.remove(itemBox));
-
-    
-        
+  class Item {
+    constructor(itemName) {
+      this.createItem(itemName);
     }
 
-    edit(input){
-        input.disabled = !input.disabled;
+    createItem(itemName) {
+      const itemBox = document.createElement("div");
+      itemBox.classList.add("item");
+
+      const input = document.createElement("input");
+      input.type = "text";
+      input.value = itemName;
+      input.disabled = true;
+      input.classList.add("item_input");
+
+      const editButton = document.createElement("button");
+      editButton.innerHTML = '<i class="fas fa-edit"></i>';
+      editButton.classList.add("editButton");
+
+      const removeButton = document.createElement("button");
+      removeButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+      removeButton.classList.add("removeButton");
+
+      container.appendChild(itemBox);
+      itemBox.appendChild(input);
+      itemBox.appendChild(editButton);
+      itemBox.appendChild(removeButton);
+
+      editButton.addEventListener("click", () => this.edit(input));
+      removeButton.addEventListener("click", () => this.remove(itemBox));
     }
 
-    remove(item){
-        container.removeChild(item);
+    edit(input) {
+      input.disabled = !input.disabled;
+      input.focus();
     }
 
-}
-
-function check(){
-    if(input.value != ""){
-        new item(input.value);
-        input.value = "";
+    remove(itemBox) {
+      container.removeChild(itemBox);
     }
-}
+  }
 
+  function check() {
+    if (input.value.trim() !== "") {
+      new Item(input.value.trim());
+      input.value = "";
+    }
+  }
 
-addButton.addEventListener('click', check);
+  addButton.addEventListener("click", check);
+  input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      check();
+    }
+  });
+});
